b10fecbb4647442f3f2aaa6f5d94a428634a98e4
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-11-19 09:20:28 +0000

    Display student details

--- student/src/components/Table.js
+++ student/src/components/Table.js
@@ -1,7 +1,10 @@
+import {students} from '../data/StudentsDb';
 
 export default function Table(){
     return(
+    
         <div className="outerDiv">
+                <h1>Students Information Portal</h1>
             <div className = "leftDiv">
         <table border="1">
            
@@ -10,8 +13,19 @@ export default function Table(){
                 <td>Last Name: </td>
                 <td>Course: </td>
                 <td>Country: </td>
+                <td>Profile</td>
             </tr>
-        
+            {
+                students.map(students=>
+                    <tr>
+                <td>{students.firstName} </td>
+                <td>{students.lastName} </td>
+                <td>{students.course} </td>
+                <td>{students.address.country} </td>
+                <td><button>View</button></td>
+            </tr> 
+                )
+            }
             
         </table>
         </div>
