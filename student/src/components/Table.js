6117c10c397ac294849db897c786e2d845b560d4
Sanduni0601 <139616844+Sanduni0601@users.noreply.github.com>
2024-11-19 08:40:18 +0000

    Create Table.js

---
+++ student/src/components/Table.js
@@ -0,0 +1,20 @@
+
+export default function Table(){
+    return(
+        <div className="outerDiv">
+            <div className = "leftDiv">
+        <table border="1">
+           
+            <tr>
+                <td>First Name: </td>
+                <td>Last Name: </td>
+                <td>Course: </td>
+                <td>Country: </td>
+            </tr>
+        
+            
+        </table>
+        </div>
+        </div>
+    );
+}
